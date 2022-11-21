67878c326eebeb60ed8122b252a5b27f161122f1
Enzo-belardo <vincenzobelardo10@gmail.com>
2022-11-21 16:16:42 +0100

    add: output

--- my.js
+++ my.js
@@ -1,7 +1,14 @@
-let chilometri= parseInt(prompt('Inserisci chilometri'));
-let anni= parseInt(prompt('Anni'));
-const prezzo= ('0.21')
+let chilometri= parseInt(prompt('inserisci chilometri'));
+let anni= parseInt(prompt('anni'));
+const prezzo= 0.21;
 let costo= prezzo * chilometri;
 console.log(costo)
 
 document.getElementById('output').innerHTML= costo;
+
+if (anni < 18){
+    costo= costo*0.8;
+}
+if (anni > 60){
+    costo= costo*0.6;
+}
